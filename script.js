0b714695c33fa6695940fdd1035b0c7e776ad865
Anton <anton20020426@gmail.com>
2024-01-27 11:38:07 +0100

    Create the reverseString() method and export it

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-export { capitalize };
+export { capitalize, reverseString };
 
 const capitalize = (str) => {
   const firstChar = str.charAt(0);
   const isCapitalized = firstChar.toUpperCase();
   return str.replace(firstChar, isCapitalized);
+};
+
+const reverseString = (str) => {
+  const arrayOfChar = str.split('');
+  return arrayOfChar.reverse().join('');
 };
