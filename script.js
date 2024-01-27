02caaaa0a7d07d321acf656a43661aed041d0a28
Anton <anton20020426@gmail.com>
2024-01-27 12:00:54 +0100

    Create the Calculator object and export it

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-export { capitalize, reverseString };
+export { capitalize, reverseString, Calculator };
 
 const capitalize = (str) => {
   const firstChar = str.charAt(0);
@@ -10,3 +10,22 @@ const reverseString = (str) => {
   const arrayOfChar = str.split('');
   return arrayOfChar.reverse().join('');
 };
+
+class Calculator {
+  static add(num1, num2) {
+    return Number(num1) + Number(num2);
+  }
+
+  static subtract(num1, num2) {
+    return num1 - num2;
+  }
+
+  static divide(num1, num2) {
+    if (!num2) return 'Cannot divide by 0';
+    else return num1 / num2;
+  }
+
+  static multiply(num1, num2) {
+    return num1 * num2;
+  }
+}
