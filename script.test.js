9c456f30d9bbee01870cdef1e79caadb563c11e2
Anton <anton20020426@gmail.com>
2024-01-27 11:26:36 +0100

    Add some tests for a reverseString() method

--- script.test.js
+++ script.test.js
@@ -1,4 +1,4 @@
-import { capitalize } from './script.js';
+import { capitalize, reverseString } from './script.js';
 
 describe('First character is capitalized', () => {
   test('in a word', () => {
@@ -7,5 +7,15 @@ describe('First character is capitalized', () => {
 
   test('in a sentence', () => {
     expect(capitalize('hello world.')).toMatch(/^[A-Z][\w\s.]+$/);
+  });
+});
+
+describe('Character order is reversed', () => {
+  test('in a word', () => {
+    expect(reverseString('sleep')).toMatch('peels');
+  });
+
+  test('in a sentence', () => {
+    expect(reverseString('I am sleepy today.')).toMatch('.yadot ypeels ma I');
   });
 });
